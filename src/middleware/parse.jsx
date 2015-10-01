5336d8477e04feddb46cdd966e9645dc961d6736
Gavri (Gabriel) Guy <gavri.guy@gmail.com>
2015-10-01 20:40:18 +0300

    order boxes by the 'order' property

--- src/middleware/parse.jsx
+++ src/middleware/parse.jsx
@@ -2,7 +2,7 @@ export default (Parse) => () => next => action => {
   switch (action.type) {
   case 'GETBOXLIST':
     const Box = Parse.Object.extend('Box');
-    const query = new Parse.Query(Box);
+    const query = new Parse.Query(Box).addAscending('order');
     query.find({
       success: (results) => {
         action.boxList = results;
