4fa68755b145c28244833651a4bea4132a6be038
Gavri (Gabriel) Guy <gavri.guy@gmail.com>
2015-10-02 21:23:27 +0300

    move-to-parse-rest-api

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var template = require('gulp-template');
 gulp.task('default', function () {
   console.log('Creating dist/index.html')
   return gulp.src('src/index.html')
-    .pipe(template({FIREBASE_APP: process.env.FIREBASE_APP}))
+    .pipe(template({
+      X_PARSE_APPLICATION_ID: process.env.X_PARSE_APPLICATION_ID,
+      X_PARSE_REST_API_KEY: process.env.X_PARSE_REST_API_KEY
+    }))
     .pipe(gulp.dest('dist'));
 });
--- src/middleware/spec.jsx
+++ src/middleware/spec.jsx
@@ -2,60 +2,25 @@
 // in the Middleware section
 
 import test from 'tape';
-import firebaseMiddleware from './firebase';
-import {HEARTBEAT_STATUSES} from '../reducer';
+import parseMiddleware from './parse';
 
-// Factory for Firenase custom mock class that mocks the 'on' method
-// The Factory takes the 'returnValue' value as parameter to conterol
-// the mocked result.
-const firebaseMockCreator = (returnValue) => {
-  return class FirebaseMock {
-    on(eventType, callback) {
-      const snapshot = {
-        val() {
-          return returnValue;
-        },
-      };
-      callback(snapshot);
-    }
-  };
+const axiosMockSuccess = () => {
+  return Promise.resolve({data: {results: []}});
 };
 
-// Taken from http://rackt.github.io/redux/docs/recipes/WritingTests.html
-const createFakeStore = fakeData => ({
-  getState() {
-    return fakeData;
-  },
-});
-
-// Inspired from http://rackt.github.io/redux/docs/recipes/WritingTests.html
-// with one expection we pass the firebase mock class as a paramter so
-// we can controlled its mocked value using firebaseMockCreator
-const dispatchWithStoreOf = (firebaseMock, storeData, action) => {
-  let dispatched = null;
-  const dispatch = firebaseMiddleware(firebaseMock)(
-    createFakeStore(storeData)
-  )(actionAttempt => dispatched = actionAttempt);
-  dispatch(action);
-  return dispatched;
+const action = {
+  type: 'GETBOXLIST',
 };
 
-test('firebaseMiddleware', (assert) => {
-  const action = {
-    type: 'HEARTBEAT',
-  };
+const next = (actionOutput) => {
+  test('parseMiddleware', (assert) => {
+    assert.deepEqual(
+      actionOutput.boxList,
+      [],
+      'Should return array of boxes.'
+    );
+    assert.end();
+  });
+};
 
-  assert.equal(
-    dispatchWithStoreOf(firebaseMockCreator(true), {}, action).status,
-    HEARTBEAT_STATUSES.OK,
-    'Should return OK when Firebases connection returns true.'
-  );
-
-  assert.equal(
-    dispatchWithStoreOf(firebaseMockCreator(false), {}, action).status,
-    HEARTBEAT_STATUSES.ERROR,
-    'Should return ERROR when Firebases connection returns false.'
-  );
-
-  assert.end();
-});
+parseMiddleware(axiosMockSuccess, {dashboardSettings: {}})()(next)(action);
